8497485f5fe717c3db45ae32ecbec6acea349d3a
kourtney reynolds <kourtney.reynolds@hidigital.solutions>
2019-11-22 09:13:45 -0700

    Adds Check for gutter ball and pins hit on ball 2

--- ScoreBoardTest/ScoreBoardTest.cs
+++ ScoreBoardTest/ScoreBoardTest.cs
@@ -16,5 +16,25 @@ namespace BowlingScoreTest
 
             actual.Should().Be(expected);
         }
+
+        [Fact]
+        public void ReturnZeroForTwoGutterBalls()
+        {
+            var frames = "--";
+            var expected = 0;
+            var actual = ScoreBoard.GetScore(frames);
+
+            actual.Should().Be(expected);
+        }
+
+        [Fact]
+        public void ReturnOneForOneGutterBallOnePin()
+        {
+            var frames = "-1";
+            var expected = 1;
+            var actual = ScoreBoard.GetScore(frames);
+
+            actual.Should().Be(expected);
+        }
     }
 }
