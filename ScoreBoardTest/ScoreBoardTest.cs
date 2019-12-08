654379a14d4b2aff2bbbeb17f58c957a9854867a
kourtney reynolds <kourtney.reynolds@hidigital.solutions>
2019-12-08 10:26:19 -0700

    Adds in test for all gutter balls and all left  open frames

--- ScoreBoardTest/ScoreBoardTest.cs
+++ ScoreBoardTest/ScoreBoardTest.cs
@@ -18,10 +18,30 @@ namespace BowlingScoreTest
         }
 
         [Fact]
+        public void ReturnsZeroForGutterBalls()
+        {
+            var game = "--|--|--|--|--|--|--|--|--|--";
+            var expected = 0;
+            var actual = ScoreBoard.GetTotalScore(game);
+
+            actual.Should().Be(expected);
+        }
+
+        [Fact]
         public void ReturnNineForAllGutterBallsExceptOneTurn()
         {
-            var game = "--|--|--|--|--|--|--|--|--|-9||";
+            var game = "--|--|--|--|--|--|--|--|--|-9";
             var expected = 9;
+            var actual = ScoreBoard.GetTotalScore(game);
+
+            actual.Should().Be(expected);
+        }
+
+        [Fact]
+        public void ReturnsNinetyForAllBallsOnSecondTurn()
+        {
+            var game = "9-|9-|9-|9-|9-|9-|9-|9-|9-|9-";
+            var expected = 90;
             var actual = ScoreBoard.GetTotalScore(game);
 
             actual.Should().Be(expected);
@@ -30,7 +50,7 @@ namespace BowlingScoreTest
         [Fact]
         public void ReturnTwentyEightForOneSpare()
         {
-            var game = "--|--|--|--|--|--|--|--|-/|9-||";
+            var game = "--|--|--|--|--|--|--|--|-/|9-";
             var expected = 28;
             var actual = ScoreBoard.GetTotalScore(game);
 
